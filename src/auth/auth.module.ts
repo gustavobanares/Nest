6b5373cb0248f05b77021c49f985bec930900a8c
*Gustavo <gustavobbacco@gmail.com>
2025-03-17 16:43:26 -0300

    feat: add private and public key and signOptions use algorithm HS256

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,20 +5,23 @@ import { ConfigService } from "@nestjs/config";
 import { Env } from "src/env";
 
 @Module({
-    imports: [
-        PassportModule,
-        JwtModule.registerAsync({
-            inject: [ConfigService],
-            useFactory(config: ConfigService<Env, true>){
-                const secret = config.get('JWT_SECRET', {infer: true})
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory(config: ConfigService<Env, true>) {
+        const privateKey = config.get("JWT_PRIVATE_KEY", { infer: true });
 
-                return {
-                    secret,
-                }
-            },
-        })
-    ]
+        const publicKey = config.get("JWT_PUBLIC_KEY", { infer: true });
+
+        return {
+          signOptions: { algorithm: "HS256" },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
+        };
+      },
+    }),
+  ],
 })
-export class AuthModule{
-
-}
+export class AuthModule {}
