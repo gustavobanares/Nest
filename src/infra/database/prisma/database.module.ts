7678a7d3c814754f867403f7fd95f32600931658
*Gustavo <gustavobbacco@gmail.com>
2025-04-01 17:31:49 -0300

    refactor: import repostiries from student

--- src/infra/database/prisma/database.module.ts
+++ src/infra/database/prisma/database.module.ts
@@ -1,14 +1,15 @@
+import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
+import { PrismaQuestionsRepository } from "./repositories/prisma-questions-repository";
+import { PrismaQuestionAttachmentsRepository } from "./repositories/prisma-question-attachments-repository";
+import { PrismaQuestionCommentsRepository } from "./repositories/prisma-question-comments-repository";
+import { PrismaService } from "./prisma.service";
+import { Module } from "@nestjs/common";
+import { PrismaAnswersRepository } from "./repositories/prisma-answers-repository";
+import { PrismaAnswerCommentsRepository } from "./repositories/prisma-answer-comments-repository";
+import { PrismaAnswerAttachmentsRepository } from "./repositories/prisma-answer-attachments-repository";
+import { StudentsRepository } from "@/domain/forum/application/repositories/students-repository";
+import { PrismaStudentsRepository } from "./repositories/prisma-students-repository";
 
-import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
-import { PrismaQuestionsRepository } from './repositories/prisma-questions-repository'
-import { PrismaQuestionAttachmentsRepository } from './repositories/prisma-question-attachments-repository'
-import { PrismaQuestionCommentsRepository } from './repositories/prisma-question-comments-repository'
-import { PrismaService } from './prisma.service'
-import { Module } from '@nestjs/common'
-import { PrismaAnswersRepository } from './repositories/prisma-answers-repository'
-import { PrismaAnswerCommentsRepository } from './repositories/prisma-answer-comments-repository'
-import { PrismaAnswerAttachmentsRepository } from './repositories/prisma-answer-attachments-repository'
- 
 @Module({
   providers: [
     PrismaService,
@@ -17,15 +18,20 @@ import { PrismaAnswerAttachmentsRepository } from './repositories/prisma-answer-
       provide: QuestionsRepository,
       useClass: PrismaQuestionsRepository,
     },
+    {
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository,
+    },
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswersRepository,
-    PrismaAnswerCommentsRepository,  // Adicionado
+    PrismaAnswerCommentsRepository, // Adicionado
     PrismaAnswerAttachmentsRepository, // Adicionado
   ],
   exports: [
     PrismaService,
     QuestionsRepository,
+    StudentsRepository,
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswersRepository,
