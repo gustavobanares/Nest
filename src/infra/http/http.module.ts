f16c779e4c2b54682d2741c056e4da47740849c2
*Gustavo <gustavobbacco@gmail.com>
2025-04-01 17:32:31 -0300

    refactor: import register, authenticate student and cryptography module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,20 +4,26 @@ import { FetchRecentQuestionsController } from "./controllers/fetch-recent-quest
 import { CreateQuestionController } from "./controllers/create-question.controller";
 import { CreateAccountController } from "./controllers/create-account.controller";
 import { AuthenticateController } from "./controllers/authenticate.controller";
-import { DatabaseModule } from '../database/prisma/database.module';
+import { DatabaseModule } from "../database/prisma/database.module";
 import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
 import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
+import { RegisterStudentUseCase } from "@/domain/forum/application/use-cases/register-student";
+import { AuthenticateStudentUseCase } from "@/domain/forum/application/use-cases/authenticate-student";
+import { CryptographyModule } from "../cryptograhy/cryptography.module";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [CreateQuestionUseCase,
-    FetchRecentQuestionsUseCase
+  providers: [
+    CreateQuestionUseCase,
+    FetchRecentQuestionsUseCase,
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase,
   ],
 })
 export class HttpModule {}
