d63cfee910b0c172c7a406b6e2bb181c897f2e37
*Gustavo <gustavobbacco@gmail.com>
2025-04-03 20:30:08 -0300

    refactor: import comment on question use case and controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -24,6 +24,8 @@ import { FetchQuestionAnswersController } from "./controllers/fetch-question-ans
 import { FetchQuestionAnswersUseCase } from "@/domain/forum/application/use-cases/fetch-questions-answers";
 import { ChooseQuestionBestAnswerController } from "./controllers/choose-question-best-answer.controller";
 import { ChooseQuestionBestAnswerUseCase } from "@/domain/forum/application/use-cases/choose-question-best-answer";
+import { CommentOnQuestionController } from "./controllers/comment-on-question.controller";
+import { CommentOnQuestionUseCase } from "@/domain/forum/application/use-cases/comment-on-question";
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -39,6 +41,7 @@ import { ChooseQuestionBestAnswerUseCase } from "@/domain/forum/application/use-
     DeleteAnswerController,
     FetchQuestionAnswersController,
     ChooseQuestionBestAnswerController,
+    CommentOnQuestionController
   ],
   providers: [
     CreateQuestionUseCase,
@@ -52,6 +55,7 @@ import { ChooseQuestionBestAnswerUseCase } from "@/domain/forum/application/use-
     DeleteAnswerUseCase,
     FetchQuestionAnswersUseCase,
     ChooseQuestionBestAnswerUseCase,
+    CommentOnQuestionUseCase
   ],
 })
 export class HttpModule {}
