1d474cc165e5d3a3874bb286824369c4721c5024
*Gustavo <gustavobbacco@gmail.com>
2025-04-10 15:26:39 -0300

    test: update attachment

--- src/domain/forum/application/use-cases/edit-answer.spec.ts
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -2,10 +2,9 @@ import { EditAnswerUseCase } from "./edit-answer";
 import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
 import { makeAnswer } from "test/factories/make-answer";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { beforeEach, describe, expect, it } from "vitest";
-import { NotAllowedError } from "@/core/errors/not-allowed-error";
 import { InMemoryAnswerAttachmentsRepository } from "test/repositories/in-memory-answer-attachments-repository";
 import { makeAnswerAttachment } from "test/factories/make-answer-attachment";
+import { NotAllowedError } from "@/core/errors/not-allowed-error";
 
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository;
 let inMemoryAnswersRepository: InMemoryAnswersRepository;
@@ -37,11 +36,11 @@ describe("Edit Answer", () => {
 
     inMemoryAnswerAttachmentsRepository.items.push(
       makeAnswerAttachment({
-        questionId: newAnswer.id,
+        answerId: newAnswer.id,
         attachmentId: new UniqueEntityID("1"),
       }),
       makeAnswerAttachment({
-        questionId: newAnswer.id,
+        answerId: newAnswer.id,
         attachmentId: new UniqueEntityID("2"),
       })
     );
@@ -87,5 +86,47 @@ describe("Edit Answer", () => {
 
     expect(result.isLeft()).toBe(true);
     expect(result.value).toBeInstanceOf(NotAllowedError);
+  });
+
+  it("should sync new and removed attachment when editing an answer", async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID("author-1"),
+      },
+      new UniqueEntityID("question-1")
+    );
+
+    await inMemoryAnswersRepository.create(newAnswer);
+
+    inMemoryAnswerAttachmentsRepository.items.push(
+      makeAnswerAttachment({
+        answerId: newAnswer.id,
+        attachmentId: new UniqueEntityID("1"),
+      }),
+      makeAnswerAttachment({
+        answerId: newAnswer.id,
+        attachmentId: new UniqueEntityID("2"),
+      })
+    );
+
+    const result = await sut.execute({
+      answerId: newAnswer.id.toValue(),
+      authorId: "author-1",
+      content: "Conteúdo teste",
+      attachmentsIds: ["1", "3"],
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(inMemoryAnswerAttachmentsRepository.items).toHaveLength(2);
+    expect(inMemoryAnswerAttachmentsRepository.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID("1"),
+        }),
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID("3"),
+        }),
+      ])
+    );
   });
 });
